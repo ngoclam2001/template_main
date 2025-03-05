b23fc6773ff5e857a8ff01b7ff7273bc7b7d7547
NgocLam <nguyenlampv2001@gmail.com>
2025-03-05 18:06:15 +0930

    check

--- template_main/Controllers/HomeController.cs
+++ template_main/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+﻿using System;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using template_main.Models;
@@ -15,9 +16,34 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-      
-        DanhSachDat dsd = new DanhSachDat(ID,);
-        return View();
+
+        var dataLand = new DataLand
+        {
+            Id = "AA 0987654",
+            Muc1 = new Dictionary<int, Person>
+                {
+                    { 1, new Person { PersonName = "Nguyễn Văn A", PersonCCCD = "231414141" } },
+                    { 2, new Person { PersonName = "Nguyễn Văn B", PersonCCCD = "231414142" } },
+                    { 3, new Person { PersonName = "Nguyễn Văn C", PersonCCCD = "231414143" } }
+                },
+            Muc2 = new Dictionary<int, Land>
+                {
+                    { 1, new Land { LandNumber = "01", LandMapNumber = "100m2", LandArea = "cac", LandClass = "abc", LandUseDate = "abc", LandUse = "abc", LandAddress = "abc", LandPurpose = "abc" } },
+                    { 2, new Land { LandNumber = "02", LandMapNumber = "200m2", LandArea = "cac", LandClass = "abc", LandUseDate = "abc", LandUse = "abc", LandAddress = "abc", LandPurpose = "abc" } }
+                },
+            Muc3 = new Dictionary<int, Asset>
+                {
+                    { 1, new Asset { AssetName = "01", AssetArea = "100m2", AssetAreaUse = "01", AssetNumberFloor = "100m2", AssetStructure = "01", AssetLevel = "100m2", AssetUse = "01", AssetUseTime = "100m2" } },
+                    { 2, new Asset { AssetName = "02", AssetArea = "200m2", AssetAreaUse = "02", AssetNumberFloor = "200m2", AssetStructure = "02", AssetLevel = "200m2", AssetUse = "02", AssetUseTime = "200m2" } }
+                },
+            Muc4 = new Dictionary<int, string>
+                {
+                    { 0, "/img_re.png" }
+                },
+            IsCheckMuc2 = true,
+            IsCheckMuc3 = true
+        };
+        return View(dataLand);
     }
 
     public IActionResult Privacy()
